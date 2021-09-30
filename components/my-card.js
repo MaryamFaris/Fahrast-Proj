c43db11a4984dc2c83566a789f6d1f65d15b8642
Maryam Al Faris <69353028+MaryamFaris@users.noreply.github.com>
2021-09-30 22:19:31 +0300

    Add files via upload

--- components/my-card.js
+++ components/my-card.js
@@ -5,11 +5,11 @@ import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import ListGroup from 'react-bootstrap/ListGroup';
 let MyCard =()=>{
     const cardInfo =[
-        {image:"https:/0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAD ", bookTitle:"The Dark Knight ", text:"kghfdjhgjdndg ", orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000", bookCount:"1", libraryName:"مكتبة بابيلون"},
-        {image:"images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAN ", bookTitle:"B ", text:" dgjhdghgf", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"4",libraryName:"مكتبة بابيلون"},
-        {image:"https://www.google.com/url?sa=i&url=https%3ICFQAAAAAdAAAAABAT ", bookTitle:"C ", text:" digjfjgdjk" , orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"3", libraryName:"مكتبة بابيلون"},
-        {image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ", text:"gjdifjgdjdgjd ", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
-        {image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ", text:"gjdifjgdjdgjd " ,orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"5", libraryName:"مكتبة بابيلون"},
+        {image:"https:/0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAD ", bookTitle:"The Dark Knight ",aurtherName: "john g. avildesn", text:"kghfdjhgjdndg ", orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000", bookCount:"1", libraryName:"مكتبة بابيلون"},
+        {image:"images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAN ", bookTitle:"B ",aurtherName: "john g. avildesn", text:" dgjhdghgf", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"4",libraryName:"مكتبة بابيلون"},
+        {image:"https://www.google.com/url?sa=i&url=https%3ICFQAAAAAdAAAAABAT ", bookTitle:"C ",aurtherName: "john g. avildesn", text:" digjfjgdjk" , orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"3", libraryName:"مكتبة بابيلون"},
+        {image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", aurtherName: "john g. avildesn", bookTitle:"D ", text:"gjdifjgdjdgjd ", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
+        {image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", aurtherName: "john g. avildesn",bookTitle:"D ", text:"gjdifjgdjdgjd " ,orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"5", libraryName:"مكتبة بابيلون"},
     ];
     let cardInfoLen= cardInfo.length;
     const renderCard = (card , index)=>{
@@ -18,16 +18,21 @@ let MyCard =()=>{
                 <Card key={index} >
             <Card.Body>
                 <div className="orderInfo">
-                <ListGroupItem>معلومات الطلب</ListGroupItem>
-                <Card>{card.orderDate} / تاريخ الطلب</Card>
-                <Card>{card.orderNo} / رقم الطلب</Card>
-                <Card>{card.priceAll} / السعر شامل التوصيل </Card>
+                <lh className="listName">معلومات الطلب</lh>
+                <ul>
+                <li>  تاريخ الطلب / {card.orderDate}</li>
+                <li>  رقم الطلب / {card.orderNo}</li>
+                <li>  السعر شامل التوصيل / {card.priceAll}</li>
+                </ul>
                 </div>
+                <hr></hr>
                 <div className="flexInfo">
                 <div className="bookImg"><Card.Img src={card.image}/></div>
                 <div className="bookInfo">
-                <Card.Title>{card.bookTitle}</Card.Title>
-                <div> العدد {card.bookCount}  / السعر {card.bookPrice} </div>
+                <div className="bookTitle">{card.bookTitle}</div>
+                <div >{card.aurtherName}</div>
+                <div>  {card.bookCount}x العدد  </div>
+                <div>  {card.bookPrice} IQ   السعر</div>
                 <Card.Text>{card.libraryName}</Card.Text>
                 </div>
                 </div>
@@ -39,10 +44,11 @@ let MyCard =()=>{
         return(<div className="card-container">
             <Card key={index}>
             <Card.Body>
-                <ListGroup className="list-group-flush">
+                <ListGroup>
                   <ListGroupItem>لم يتم حجز كتاب</ListGroupItem>
                 </ListGroup>
             </Card.Body>
+            
         </Card>
         </div>
             )}
--- components/orders.js
+++ components/orders.js
@@ -6,11 +6,11 @@ import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import ListGroup from 'react-bootstrap/ListGroup';
 let MyOrders =()=>{
     const cardInfo =[
-        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", image:"https:/0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAD ", bookTitle:"The Dark Knight ", orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000", bookCount:"1", libraryName:"مكتبة بابيلون"},
-        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي",  image:"images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAN ", bookTitle:"B ", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"4",libraryName:"مكتبة بابيلون"},
-        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", image:"https://www.google.com/url?sa=i&url=https%3ICFQAAAAAdAAAAABAT ", bookTitle:"C ",  orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"3", libraryName:"مكتبة بابيلون"},
-        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ",  orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
-        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ",  orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"5", libraryName:"مكتبة بابيلون"},
+        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي",aurtherName: "john g. avildesn", image:"https:/0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAD ", bookTitle:"The Dark Knight ", orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000", bookCount:"1", libraryName:"مكتبة بابيلون"},
+        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", aurtherName: "john g. avildesn", image:"images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAN ", bookTitle:"B ", orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"1",libraryName:"مكتبة بابيلون"},
+        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", aurtherName: "john g. avildesn",image:"https://www.google.com/url?sa=i&url=https%3ICFQAAAAAdAAAAABAT ", bookTitle:"C ",  orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
+        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", aurtherName: "john g. avildesn",image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ",  orderNo:"123", orderDate: "15/1/2020", bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
+        {customerName:"Ahmed", phoneNo:"075138890139", address:"الموصل حي الزراعي", aurtherName: "john g. avildesn",image:"7294000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOD8jdK02PICFQAAAAAdAAAAABAT ", bookTitle:"D ",  orderNo:"123", orderDate: "15/1/2020",bookPrice:"10000" ,priceAll: "3000",  bookCount:"1", libraryName:"مكتبة بابيلون"},
     ];
     let cardInfoLen= cardInfo.length;
     const renderCard = (card , index)=>{
@@ -21,24 +21,27 @@ let MyOrders =()=>{
             <button className="icon" onClick={()=>console.log('Closed')} title="اضافة الى منجز"><IoEllipsisHorizontalSharp /></button>
             <button className="icon" onClick={()=>console.log('Closed')} title="حذف الطلب"><IoTrashOutline /></button>
                 <div className="orderInfo">
-                <ListGroupItem>معلومات الطلب</ListGroupItem>
-                <ListGroup><Card>{card.customerName} / اسم الزبون </Card>
-                <Card>{card.orderDate} / تاريخ الطلب</Card>
-                <Card>{card.phoneNo} / رقم الهاتف </Card>
-                <Card>{card.orderNo} / رقم الطلب</Card>
-                <Card>{card.priceAll} / السعر شامل التوصيل </Card>
-                <Card>  العنوان / {card.address}</Card>
-                </ListGroup>
+                <lh className="listName">معلومات الطلب</lh>
+                <ul>
+                <li>اسم الزبون  /  {card.customerName}  </li>
+                <li>  تاريخ الطلب / {card.orderDate}</li>
+                <li>  رقم الهاتف  / {card.phoneNo}</li>
+                <li>  رقم الطلب / {card.orderNo}</li>
+                <li>  السعر شامل التوصيل / {card.priceAll}</li>
+                <li>  العنوان / {card.address}</li>
+                </ul>
                 </div>
+                <hr></hr>
                 <div className="flexInfo">
                 <div className="bookImg"><Card.Img src={card.image}/></div>
                 <div className="bookInfo">
-                <Card.Title>{card.bookTitle}</Card.Title>
-                <div> العدد {card.bookCount}  / السعر {card.bookPrice} </div>
+                <div className="bookTitle">{card.bookTitle}</div>
+                <div >{card.aurtherName}</div>
+                <div>  {card.bookCount}x العدد  </div>
+                <div>  {card.bookPrice} IQ   السعر</div>
                 <Card.Text>{card.libraryName}</Card.Text>
                 </div>
                 </div>
-               
             </Card.Body>
         </Card>
         </div>
